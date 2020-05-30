43d97a1ce0650f9b59d2a95fc26f1c0db8b0e19b
vapao <live1989@foxmail.com>
2020-05-30 18:01:07 +0800

    A 添加google分析

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -108,6 +108,9 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   repoUrl: 'https://github.com/openspug/spug',
+
+  gaTrackingId: 'UA-167996677-1',
+  gaGtag: true
 };
 
 module.exports = siteConfig;
